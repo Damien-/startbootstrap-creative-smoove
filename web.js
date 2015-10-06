54c5abdb9948423b97b09c906f4759c77351096c
Damien <damnjan.nikolic@gmail.com>
2015-10-06 15:13:18 +0100

    updated

--- web.js
+++ web.js
@@ -4,6 +4,13 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use('/img', express.static(__dirname + '/img'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/less', express.static(__dirname + '/less'));
+app.use('/fonts', express.static(__dirname + '/fonts'));
+app.use('/font-awesome', express.static(__dirname + '/font-awesome'));
+
 app.get('/', function(request, res){
   content = fs.readFileSync('./index.html').toString();
   res.send(content);
