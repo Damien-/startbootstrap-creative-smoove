72cb4e1dd0da03a77a926880ae1be7569ddbb7ea
Damien <damnjan.nikolic@gmail.com>
2015-10-06 14:38:23 +0100

    node js

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var content;
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, res){
+  content = fs.readFileSync('./index.html').toString();
+  res.send(content);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
